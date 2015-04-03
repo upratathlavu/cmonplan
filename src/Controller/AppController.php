44a9982b97fe1d64075218d33fc8ea0a6e99b6e3
upratathlavu <username@users.noreply.github.com>
2015-04-04 00:54:13 +0200

    ...

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -51,8 +51,14 @@ class AppController extends Controller
         ]);
     }
 
-    public function beforeFilter(Event $event)
-    {
-        $this->Auth->allow(['index', 'view', 'display']);
-    }
+	public function isAuthorized($user)
+	{
+		// Admin can access every action
+		if (isset($user['role']) && $user['role'] === 1) {
+			return true;
+		}
+
+		// Default deny
+		return false;
+	}
 }
