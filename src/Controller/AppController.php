7bce4640235267fe3e3c4f4fb8cd4a9f247191fe
upratathlavu <username@users.noreply.github.com>
2015-04-04 02:03:20 +0200

    ...

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -49,7 +49,7 @@ class AppController extends Controller
                 'controller' => 'Users',
                 'action' => 'login'
             ],
-			'unauthorizedRedirect' => $this->request->url()
+			'unauthorizedRedirect' => $this->referer()
 			//'unauthorizedRedirect' => [
 			//	'controller' => 'Needs',
 			//	'action' => 'index'
