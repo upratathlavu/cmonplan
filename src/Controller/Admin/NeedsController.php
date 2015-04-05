e25800a523f4268d378cc86f3faef58d98b5ca2b
upratathlavu <username@users.noreply.github.com>
2015-04-05 16:49:52 +0200

    ...

--- src/Controller/Admin/NeedsController.php
+++ src/Controller/Admin/NeedsController.php
@@ -49,8 +49,9 @@ class NeedsController extends AppController
         $need = $stmt->fetch('assoc');
         
         //$need = $this->Needs->query('select u.id, u.username from needs as n join users as u on n.user_id = u.id where u.id = 1');
-        $this->set('user_id', $need['id']);
-        $this->set('username', $need['username']);
+        //$this->set('user_id', $need['id']);
+        //$this->set('username', $need['username']);
+        $this->set('need', $need);
         //$this->set('product', $need['product']);
         //$this->set('id', $id);
     }
