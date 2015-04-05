09f25870bd0d6c175a6d77f83dda6f5c56786b89
upratathlavu <username@users.noreply.github.com>
2015-04-05 16:39:02 +0200

    ...

--- src/Controller/Admin/NeedsController.php
+++ src/Controller/Admin/NeedsController.php
@@ -49,7 +49,7 @@ class NeedsController extends AppController
         $need = $stmt->fetch();
         
         //$need = $this->Needs->query('select * from needs where id = 1');
-        $this->set('username', $need);
+        $this->set('username', $need['username']);
         //$this->set('product', $need['product']);
         //$this->set('id', $id);
     }
