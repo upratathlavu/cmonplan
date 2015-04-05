9977358a4ead3e08fae1838a31777290434f2074
upratathlavu <username@users.noreply.github.com>
2015-04-05 18:33:29 +0200

    ...

--- src/Controller/Admin/NeedsController.php
+++ src/Controller/Admin/NeedsController.php
@@ -67,7 +67,7 @@ class NeedsController extends AppController
         if ($this->request->is('post')) {
 			// prerobit?
             $need = $this->Needs->patchEntity($need, $this->request->data);
-            $this->log($need, 'debug');
+            $this->log($this->request->data, 'debug');
             // prerobit?
             if ($this->Needs->save($need)) {
                 $this->Flash->success('The need has been saved.');
