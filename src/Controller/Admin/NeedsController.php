dba77da943222f941be7f6cd7a156867288ad2f5
upratathlavu <username@users.noreply.github.com>
2015-04-05 23:53:01 +0200

    ...

--- src/Controller/Admin/NeedsController.php
+++ src/Controller/Admin/NeedsController.php
@@ -121,7 +121,7 @@ class NeedsController extends AppController
         
         $conn = ConnectionManager::get('default');
         $stmt = $conn->execute(
-			'select n.id n_id, n.creation_date n_creation_date, n.quantity n_quantity, u.id u_id, u.username u_username, p.id p_id, p.name p_name from needs as n 
+			'select n.creation_date creation_date, n.quantity quantity, u.id user_id, u.username user, p.id product_id, p.name product from needs as n 
 			join users as u on n.user_id = u.id 
 			join products p on n.product_id = p.id 
 			where u.id = ?', 
