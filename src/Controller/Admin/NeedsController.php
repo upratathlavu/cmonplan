59a3678ae84d5db7a14c7d106f2b2b80df977792
upratathlavu <username@users.noreply.github.com>
2015-04-05 19:36:24 +0200

    ...

--- src/Controller/Admin/NeedsController.php
+++ src/Controller/Admin/NeedsController.php
@@ -86,9 +86,9 @@ class NeedsController extends AppController
         // prerobit
         //$users = $this->Needs->Users->find('list', ['limit' => 200]);
         //$products = $this->Needs->Products->find('list', ['limit' => 200]);
-        $stmt = $conn->execute('select username from users');
+        $stmt = $conn->execute('select id, username from users');
         $users = $stmt->fetchAll();
-        $stmt = $conn->execute('select name from products');
+        $stmt = $conn->execute('select id, name from products');
         $products = $stmt->fetchAll();
         $this->set(compact('need', 'users', 'products'));
         $this->set('_serialize', ['need']);
