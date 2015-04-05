17f8be71903e0ca79af3d08a3c75bfb54a3cd661
upratathlavu <username@users.noreply.github.com>
2015-04-05 20:03:55 +0200

    ...

--- src/Controller/Admin/NeedsController.php
+++ src/Controller/Admin/NeedsController.php
@@ -90,7 +90,7 @@ class NeedsController extends AppController
         $users = $stmt->fetchAll('assoc');
         $stmt = $conn->execute('select id, name from products');
         $products = $stmt->fetchAll('assoc');
-        $this->set('users', compact($users));
+        $this->set(compact($users));
         $this->set('products', $products);
         $this->set('need', $need);
         //$this->set(compact('need', 'users', 'products'));
