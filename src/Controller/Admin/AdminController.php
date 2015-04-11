cae00d3245936acda920edfb68afa28d8c2d9056
upratathlavu <username@users.noreply.github.com>
2015-04-11 22:42:35 +0200

    ...

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,6 +2,9 @@
 namespace App\Controller\Admin;
 
 use App\Controller\AppController;
+use Cake\Datasource\ConnectionManager;
+use Cake\Database\Connection;
+
 
 class AdminController extends AppController
 {
